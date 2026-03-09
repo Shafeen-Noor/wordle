f1dea33b0c92a1453bfbf6c6f4c4cdfe413d3b50
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-03-09 21:24:22 +0100

    Fix for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-
 import App from "./App.tsx";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Cannot find #root");
 
-const params = new URLSearchParams(window.location.search);
-const redirectPath = params.get("path");
-if (redirectPath) {
+const redirect = sessionStorage.getItem("redirect");
+if (redirect) {
+  sessionStorage.removeItem("redirect");
   window.history.replaceState(
     null,
     "",
-    import.meta.env.BASE_URL + redirectPath.slice(1),
+    import.meta.env.BASE_URL + redirect.slice(1),
   );
 }
-
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/Shafeen-Noor/wordle.git",
+  base: "/wordle/",
   plugins: [react()],
   test: {
     environment: "jsdom",
