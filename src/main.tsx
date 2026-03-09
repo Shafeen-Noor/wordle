b180e424b2d2aba4f2b2c640ab680931fe7694a3
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-03-09 21:17:14 +0100

    404 page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,16 @@ import App from "./App.tsx";
 const root = document.getElementById("root");
 if (!root) throw new Error("Cannot find #root");
 
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("path");
+if (redirectPath) {
+  window.history.replaceState(
+    null,
+    "",
+    import.meta.env.BASE_URL + redirectPath.slice(1),
+  );
+}
+
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
